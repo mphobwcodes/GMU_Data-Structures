4cbac395e411d52c9a46581cb2f20605284f7496
mphobwcodes <mpho@mathcraft.com>
2020-02-04 17:20:26 -0500

    done

---
+++ Assignment1/src/Node.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Mpho
+ * This is a single entry in a linked list.
+ * It stores a key/value pair of strings and a next pointer
+ *
+ */
+public class Node {
+	/* Node key */
+	
+	private String value;
+	
+	//Node value 
+
+}
